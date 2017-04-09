23c320b1d50fc346e40de5bef3185e8b74ff2117
Fonoster Team <fonosterteam@fonoster.com>
2017-04-09 13:39:28 -0400

    Code refactored and fixed editor (blank page)

--- webui/src/main/webapp/app/init.js
+++ webui/src/main/webapp/app/init.js
@@ -1,5 +1,13 @@
+// Stuff that can wait
+loadCSS('app/node_modules/angular-material/angular-material.min.css');
+loadCSS('app/node_modules/angular-material-data-table/dist/md-data-table.min.css');
+loadCSS('app/node_modules/tour/dist/tour.css')
+loadCSS('/static/assets/css/main.css');
+loadCSS('/static/assets/css/editor.css');
+loadCSS('app/node_modules/roboto-fontface/css/roboto/roboto-fontface.css');
+loadCSS('/static/assets/fonts/fonts.css');
+loadCSS('https://fonts.googleapis.com/icon?family=Material+Icons');
+
 // Run app
 angular.bootstrap(document, ['fonoster']);
 
-loadCSS('app/node_modules/angular-material/angular-material.min.css');
-loadCSS('app/node_modules/angular-material-data-table/dist/md-data-table.min.css');
