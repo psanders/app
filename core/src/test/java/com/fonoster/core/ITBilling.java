c32e694b5b31e5833f9e137ceecae100d6fe5e0b
Fonoster Team <fonosterteam@fonoster.com>
2017-07-04 20:07:24 -0400

    Refactored

--- core/src/test/java/com/fonoster/core/ITBilling.java
+++ core/src/test/java/com/fonoster/core/ITBilling.java
@@ -1,9 +1,9 @@
 package com.fonoster.core;
 
 import com.fonoster.core.api.BillingAPI;
-import com.fonoster.core.api.DIDsAPI;
+import com.fonoster.core.api.DIDNumbersAPI;
 import com.fonoster.exception.ApiException;
-import com.fonoster.model.DID;
+import com.fonoster.model.DIDNumber;
 import com.fonoster.model.Rate;
 import com.fonoster.model.ServiceProvider;
 import org.junit.Test;
@@ -16,18 +16,18 @@ public class ITBilling {
 
     @Test(expected = ApiException.class)
     public void rateNoMatch() throws Exception {
-        ServiceProvider sp = DIDsAPI.getInstance().getServiceProviders().get(0);
+        ServiceProvider sp = DIDNumbersAPI.getInstance().getServiceProviders().get(0);
         Rate r = BillingAPI.getInstance().getRate(sp, "02121");
     }
 
     @Test
     public void testPrice() throws Exception {
         // Get the rate to terminate a call to X number from Y service provider.
-        ServiceProvider sp = DIDsAPI.getInstance().getServiceProviders().get(0);
+        ServiceProvider sp = DIDNumbersAPI.getInstance().getServiceProviders().get(0);
         Rate rate = BillingAPI.getInstance().getRate(sp, "17853178070");
 
         // Get the price for 60 seconds calling from our test number to psanders phone
-        DID did = DIDsAPI.getInstance().getDID("+17066041487");
+        DIDNumber did = DIDNumbersAPI.getInstance().getDIDNumber("+17066041487");
         BigDecimal price = BillingAPI.getInstance().getPrice(did, "17853178070", 60);
 
         assertTrue("The selling price and the price should match", price.doubleValue() == rate.getSelling().doubleValue());
