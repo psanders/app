24e6bd74b7932d4223efb958f8a3168ef23166af
Fonoster Team <fonosterteam@fonoster.com>
2017-07-06 21:45:46 -0400

    fixed typo

--- webui/src/main/webapp/app/components/calls/calls.ctrl.js
+++ webui/src/main/webapp/app/components/calls/calls.ctrl.js
@@ -41,7 +41,7 @@ import * as moment from 'moment-timezone';
             .catch(function(error) {
                 // Ignore 404 errors
                 if (error.status != 404)
-                    console.warn(JSON.stringify(error));
+                    console.log(JSON.stringify(error));
             });
 
             self.filter = false;
