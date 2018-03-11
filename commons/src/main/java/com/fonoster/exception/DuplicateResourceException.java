91d12b36a3c0aeeb59e194c64caa8f5e11408472
Fonoster Team <fonosterteam@fonoster.com>
2018-03-11 10:03:56 -0400

    Added new exceptions

---
+++ commons/src/main/java/com/fonoster/exception/DuplicateResourceException.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (C) 2017 <fonosterteam@fonoster.com> https://fonoster.com
+ *
+ * <p>This file is part of Fonoster
+ *
+ * <p>Fonoster can not be copied and/or distributed without the express permission of Fonoster's
+ * copyright owners.
+ */
+package com.fonoster.exception;
+
+import com.fonoster.annotations.Since;
+
+@Since("1.0")
+public class DuplicateResourceException extends ApiException {
+
+  private static final long serialVersionUID = 1L;
+
+  public DuplicateResourceException() {
+  }
+
+  public DuplicateResourceException(String message) {
+    super(message);
+  }
+}
