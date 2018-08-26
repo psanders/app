0492cfe7785ef2b117664fa9fdd9bb871b4af096
Fonoster Team <fonosterteam@fonoster.com>
2018-08-26 18:13:30 -0400

    Refactored

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+#set -e
+
+PACK=''
+
+confirm() {
+    # call with a prompt string or use a default
+    read -r -p "${1:-Is finall build? [y/N]} " response
+    case "$response" in
+        [yY][eE][sS]|[yY])
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+
+confirm && PACK='-p' && echo 'This might take a while. Go grab some coffee  :)'
+
+WORKDIR=$PWD
+cd webui/src/main/webapp/app && npm update && webpack $PACK
+cd $WORKDIR/app
+gradle clean goJF jar war shadowJar
